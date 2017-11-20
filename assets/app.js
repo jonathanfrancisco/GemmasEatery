601f8d86bcdef01e84da6bc4f8892ec78fcabd66
jonathanfrancisco <jonathanfrancisco999@gmail.com>
2017-11-20 13:09:00 +0800

    Added overlay message

---
+++ assets/app.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+
+function processForm() {
+
+    const overlay = document.querySelector('.after-messsage-overlay');
+    let firstName = document.getElementById('first-name').value;
+    let message = document.querySelector('.message').innerHTML = `Thank you, ${firstName}! for getting in touch with us! We will get back to you soon.`;
+
+    overlay.style.display = "block";
+
+}
+
+function closeOverlay() {
+
+    const overlay = document.querySelector('.after-messsage-overlay');
+    overlay.style.display = "none";
+
+    const inputs = document.querySelectorAll('input, textarea');
+    inputs.forEach(function(node) {
+        node.value = "";
+    });
+
+}
